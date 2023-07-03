92e776f9f098c9aed4ee89431dd2f87f5c02a328
вадим <funpalka1@gmail.com>
2023-07-03 20:59:48 +0400

    Initional commit

---
+++ lection_3.py
@@ -0,0 +1,15 @@
+def sum_numbers(n):
+    summa = 0
+    for i in range(1, n+1):
+        summa += i
+    return summa 
+
+print(sum_numbers(5))
+
+
+def sum_str(*args):
+    res = ''
+    for i in args:
+        res+= i 
+    return res
+print(sum_str('q','e', 'l'))
